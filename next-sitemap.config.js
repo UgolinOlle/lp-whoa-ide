032163f118569fcc2fe9f46b54aa64ca6a00d27e
Ugolin O. <hello@ugolin-olle.com>
2025-05-19 16:43:27 +0200

    feat(core): adding next-sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://ton-domaine.com",
+  generateRobotsTxt: true,
+  generateIndexSitemap: true,
+  sitemapSize: 5000,
+};
